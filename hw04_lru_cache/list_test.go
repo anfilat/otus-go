e2d97ace7aa83376ad9574d71fedae617cea36a8
Andrey Filatkin <anfilat@mail.ru>
2020-09-23 20:34:26 +0300

    HW4

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,94 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("push front to empty list", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+	})
+
+	t.Run("push back to empty list", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+	})
+
+	t.Run("remove first item", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+		l.PushBack(40)
+
+		require.Equal(t, 10, l.Front().Value)
+		l.Remove(l.Front())
+		require.Equal(t, 20, l.Front().Value)
+
+		elems := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			elems = append(elems, i.Value.(int))
+		}
+		require.Equal(t, []int{20, 30, 40}, elems)
+	})
+
+	t.Run("remove last item", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+		l.PushBack(40)
+
+		require.Equal(t, 40, l.Back().Value)
+		l.Remove(l.Back())
+		require.Equal(t, 30, l.Back().Value)
+
+		elems := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			elems = append(elems, i.Value.(int))
+		}
+		require.Equal(t, []int{10, 20, 30}, elems)
+	})
+
+	t.Run("remove only item", func(t *testing.T) {
+		l := NewList()
+
+		item := l.PushFront(10)
+		l.Remove(item)
+
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
+
+	t.Run("swap first and second items", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+
+		require.Equal(t, 20, l.Front().Next.Value)
+		require.Equal(t, 10, l.Back().Prev.Value)
+
+		l.MoveToFront(l.Back())
+
+		require.Equal(t, 10, l.Front().Next.Value)
+		require.Equal(t, 20, l.Back().Prev.Value)
+
+		elems := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			elems = append(elems, i.Value.(int))
+		}
+		require.Equal(t, []int{20, 10}, elems)
+	})
 }
