bbc6c1eadbe49161cfd7ce4726c9b9cefba2e261
Andrey Filatkin <anfilat@mail.ru>
2020-11-18 10:06:31 +0300

    HW09

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,58 @@
 package hw09_struct_validator //nolint:golint,stylecheck
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	IncorrectKind      = iota // на валидацию передана не структура
+	IncorrectFieldType        // валидация неподдерживаемого типа поля (не int, string, []int, []string)
+	UnknownRule               // неизвестное правило валидации
+	IncorrectCondition        // не удается разпарсить параметр валидатора
+)
+
+// все ошибки неправильного применения валидатора.
+type ErrIncorrectUse struct {
+	reason int
+	kind   reflect.Kind
+	field  string
+	rule   string
+	err    error
+}
+
+func (e ErrIncorrectUse) Error() string {
+	switch e.reason {
+	case IncorrectKind:
+		return fmt.Sprintf("function only accepts structs; got %s", e.kind)
+	case IncorrectFieldType:
+		return fmt.Sprintf("field `%s` has unsupported type %s", e.field, e.kind)
+	case UnknownRule:
+		return fmt.Sprintf("field `%s` has unknown rule `%s`", e.field, e.rule)
+	case IncorrectCondition:
+		return fmt.Sprintf("field `%s` has incorrect condition: `%s` for rule `%s`", e.field, e.err, e.rule)
+	default:
+		return ""
+	}
+}
+
+func (e ErrIncorrectUse) Unwrap() error {
+	return e.err
+}
+
+// ошибки, обнаруживаемые валидатором.
+
+var ErrStrLen = errors.New("string length is not equal to required")
+var ErrStrRegexp = errors.New("string does not match the regexp")
+var ErrStrIn = errors.New("string is not included in the specified set")
+var ErrIntMin = errors.New("number is less than specified")
+var ErrIntMax = errors.New("number is greater than specified")
+var ErrIntIn = errors.New("number is not included in the specified set")
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +61,303 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var b strings.Builder
+	b.WriteString("validation errors: ")
+	for _, err := range v {
+		b.WriteString(err.Field)
+		b.WriteString(": ")
+		b.WriteString(err.Err.Error())
+		b.WriteString("; ")
+	}
+	return b.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here
-	return nil
+	if v == nil {
+		return nil
+	}
+
+	value := reflect.Indirect(reflect.ValueOf(v))
+	if value.Kind() != reflect.Struct {
+		return ErrIncorrectUse{reason: IncorrectKind, kind: value.Kind()}
+	}
+
+	var errs ValidationErrors
+	for i := 0; i < value.NumField(); i++ {
+		fieldType := value.Type().Field(i)
+		validateTag := fieldType.Tag.Get("validate")
+		if validateTag == "" {
+			continue
+		}
+
+		fieldValue := value.Field(i)
+		var err error
+		errs, err = validateField(errs, fieldType, fieldValue, validateTag)
+		if err != nil {
+			return err
+		}
+	}
+
+	if errs == nil {
+		return nil
+	}
+	return errs
+}
+
+func validateField(errs ValidationErrors, typ reflect.StructField, value reflect.Value, validateTag string) (ValidationErrors, error) {
+	kind := typ.Type.Kind()
+	name := typ.Name
+
+	switch kind {
+	case reflect.String:
+		rules, err := fillStrValidateRules(validateTag, name)
+		if err != nil {
+			return nil, err
+		}
+		errs = rules.check(errs, name, value)
+	case reflect.Int:
+		rules, err := fillIntValidateRules(validateTag, name)
+		if err != nil {
+			return nil, err
+		}
+		errs = rules.check(errs, name, value)
+	case reflect.Slice:
+		sliceKind := typ.Type.Elem().Kind()
+		switch sliceKind {
+		case reflect.String:
+			rules, err := fillStrValidateRules(validateTag, name)
+			if err != nil {
+				return nil, err
+			}
+			errs = rules.checkSlice(errs, name, value)
+		case reflect.Int:
+			rules, err := fillIntValidateRules(validateTag, name)
+			if err != nil {
+				return nil, err
+			}
+			errs = rules.checkSlice(errs, name, value)
+		default:
+			return nil, ErrIncorrectUse{reason: IncorrectFieldType, field: name, kind: sliceKind}
+		}
+	default:
+		return nil, ErrIncorrectUse{reason: IncorrectFieldType, field: name, kind: kind}
+	}
+	return errs, nil
+}
+
+// требование линтера вручную оптимизировать структуру по выравниванию - странное, это работа компилятора
+//nolint:maligned
+type strValidateRules struct {
+	lenIs     bool
+	lenVal    int
+	regexpIs  bool
+	regexpVal *regexp.Regexp
+	inIs      bool
+	inVal     []string
+}
+
+func fillStrValidateRules(value, name string) (*strValidateRules, error) {
+	strs := strings.Split(value, "|")
+	rules := &strValidateRules{}
+	for _, str := range strs {
+		pair := strings.SplitN(str, ":", 2)
+		if len(pair) != 2 {
+			return nil, ErrIncorrectUse{reason: IncorrectCondition, field: name, rule: str}
+		}
+		ruleName := pair[0]
+		ruleValue := pair[1]
+		switch ruleName {
+		case "len":
+			val, err := strconv.Atoi(ruleValue)
+			if err != nil {
+				return nil, ErrIncorrectUse{reason: IncorrectCondition, field: name, rule: ruleName, err: err}
+			}
+			rules.lenIs = true
+			rules.lenVal = val
+		case "regexp":
+			rg, err := regexp.Compile(ruleValue)
+			if err != nil {
+				return nil, ErrIncorrectUse{reason: IncorrectCondition, field: name, rule: ruleName, err: err}
+			}
+			rules.regexpIs = true
+			rules.regexpVal = rg
+		case "in":
+			values := strings.Split(ruleValue, ",")
+			rules.inIs = true
+			rules.inVal = values
+		default:
+			return nil, ErrIncorrectUse{reason: UnknownRule, field: name, rule: ruleName}
+		}
+	}
+	return rules, nil
+}
+
+func (r *strValidateRules) checkSlice(errs ValidationErrors, name string, value reflect.Value) ValidationErrors {
+	for i := 0; i < value.Len(); i++ {
+		errs = r.check(errs, name, value.Index(i))
+	}
+	return errs
+}
+
+func (r *strValidateRules) check(errs ValidationErrors, name string, value reflect.Value) ValidationErrors {
+	val := value.String()
+	errs = r.checkLen(errs, name, val)
+	errs = r.checkRegexp(errs, name, val)
+	errs = r.checkIn(errs, name, val)
+	return errs
+}
+
+func (r *strValidateRules) checkLen(errs ValidationErrors, name, value string) ValidationErrors {
+	if !r.lenIs {
+		return errs
+	}
+	if len(value) == r.lenVal {
+		return errs
+	}
+	return append(errs, ValidationError{Field: name, Err: ErrStrLen})
+}
+
+func (r *strValidateRules) checkRegexp(errs ValidationErrors, name, value string) ValidationErrors {
+	if !r.regexpIs {
+		return errs
+	}
+	if r.regexpVal.MatchString(value) {
+		return errs
+	}
+	return append(errs, ValidationError{Field: name, Err: ErrStrRegexp})
+}
+
+func (r *strValidateRules) checkIn(errs ValidationErrors, name, value string) ValidationErrors {
+	if !r.inIs {
+		return errs
+	}
+	if stringContains(r.inVal, value) {
+		return errs
+	}
+	return append(errs, ValidationError{Field: name, Err: ErrStrIn})
+}
+
+// требование линтера вручную оптимизировать структуру по выравниванию - странное, это работа компилятора
+//nolint:maligned
+type intValidateRules struct {
+	minIs  bool
+	minVal int
+	maxIs  bool
+	maxVal int
+	inIs   bool
+	inVal  []int
+}
+
+func fillIntValidateRules(value, name string) (*intValidateRules, error) {
+	strs := strings.Split(value, "|")
+	rules := &intValidateRules{}
+	for _, str := range strs {
+		pair := strings.Split(str, ":")
+		if len(pair) != 2 {
+			return nil, ErrIncorrectUse{reason: IncorrectCondition, field: name, rule: str}
+		}
+		ruleName := pair[0]
+		ruleValue := pair[1]
+		switch ruleName {
+		case "min":
+			val, err := strconv.Atoi(ruleValue)
+			if err != nil {
+				return nil, ErrIncorrectUse{reason: IncorrectCondition, field: name, rule: ruleName, err: err}
+			}
+			rules.minIs = true
+			rules.minVal = val
+		case "max":
+			val, err := strconv.Atoi(ruleValue)
+			if err != nil {
+				return nil, ErrIncorrectUse{reason: IncorrectCondition, field: name, rule: ruleName, err: err}
+			}
+			rules.maxIs = true
+			rules.maxVal = val
+		case "in":
+			values, err := strsToInts(strings.Split(ruleValue, ","))
+			if err != nil {
+				return nil, ErrIncorrectUse{reason: IncorrectCondition, field: name, rule: ruleName, err: err}
+			}
+			rules.inIs = true
+			rules.inVal = values
+		default:
+			return nil, ErrIncorrectUse{reason: UnknownRule, rule: ruleName}
+		}
+	}
+	return rules, nil
+}
+
+func (r *intValidateRules) checkSlice(errs ValidationErrors, name string, value reflect.Value) ValidationErrors {
+	for i := 0; i < value.Len(); i++ {
+		errs = r.check(errs, name, value.Index(i))
+	}
+	return errs
+}
+
+func (r *intValidateRules) check(errs ValidationErrors, name string, value reflect.Value) ValidationErrors {
+	val := int(value.Int())
+	errs = r.checkMin(errs, name, val)
+	errs = r.checkMax(errs, name, val)
+	errs = r.checkIn(errs, name, val)
+	return errs
+}
+
+func (r *intValidateRules) checkMin(errs ValidationErrors, name string, value int) ValidationErrors {
+	if !r.minIs {
+		return errs
+	}
+	if value >= r.minVal {
+		return errs
+	}
+	return append(errs, ValidationError{Field: name, Err: ErrIntMin})
+}
+
+func (r *intValidateRules) checkMax(errs ValidationErrors, name string, value int) ValidationErrors {
+	if !r.maxIs {
+		return errs
+	}
+	if value <= r.maxVal {
+		return errs
+	}
+	return append(errs, ValidationError{Field: name, Err: ErrIntMax})
+}
+
+func (r *intValidateRules) checkIn(errs ValidationErrors, name string, value int) ValidationErrors {
+	if !r.inIs {
+		return errs
+	}
+	if intContains(r.inVal, value) {
+		return errs
+	}
+	return append(errs, ValidationError{Field: name, Err: ErrIntIn})
+}
+
+func stringContains(slice []string, str string) bool {
+	for _, v := range slice {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+func intContains(slice []int, str int) bool {
+	for _, v := range slice {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+func strsToInts(strs []string) ([]int, error) {
+	result := make([]int, 0, len(strs))
+	for _, v := range strs {
+		val, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, val)
+	}
+	return result, nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -3,7 +3,12 @@ package hw09_struct_validator //nolint:golint,stylecheck
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -34,23 +39,242 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	Shape struct {
+		Width  float64 `validate:"min:10"`
+		Height float64 `validate:"max:50"`
+	}
+
+	Roulette struct {
+		Value int `validate:"odd:true"`
+	}
+
+	Piece struct {
+		Text string `validate:"spell:true"`
+	}
+
+	WrongCond1 struct {
+		Value string `validate:"len:oops"`
+	}
+
+	WrongCond2 struct {
+		Value string `validate:"regexp:+"`
+	}
+
+	WrongCond3 struct {
+		Value int `validate:"min:oops"`
+	}
+
+	WrongCond4 struct {
+		Value int `validate:"min"`
+	}
 )
 
-func TestValidate(t *testing.T) {
+func TestValidateSuccess(t *testing.T) {
 	tests := []struct {
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here
+			in:          nil,
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here
+		{
+			in: User{
+				ID:     "012345678901234567890123456789123456",
+				Name:   "Somebody",
+				Age:    20,
+				Email:  "test@mail.ru",
+				Role:   "admin",
+				Phones: []string{"79270000000"},
+				meta:   []byte("{}"),
+			},
+			expectedErr: nil,
+		},
+		{
+			in: &User{
+				ID:     "012345678901234567890123456789123456",
+				Name:   "Somebody",
+				Age:    20,
+				Email:  "test@mail.ru",
+				Role:   "admin",
+				Phones: []string{"79270000000"},
+				meta:   []byte("{}"),
+			},
+			expectedErr: nil,
+		},
+		{
+			in: App{
+				Version: "12345",
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Token{
+				Header:    []byte("12345"),
+				Payload:   []byte("12345"),
+				Signature: []byte("12345"),
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Response{
+				Code: 200,
+				Body: "content",
+			},
+			expectedErr: nil,
+		},
 	}
 
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			// Place your code here
+			err := Validate(tt.in)
+			require.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
+
+const wrongRegexp = "+"
+
+func TestValidateFail(t *testing.T) {
+	_, atoiErr := strconv.Atoi("oops")
+	_, regexpErr := regexp.Compile(wrongRegexp)
+
+	tests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in:          "text",
+			expectedErr: ErrIncorrectUse{reason: IncorrectKind, kind: reflect.String},
+		},
+		{
+			in: Shape{
+				Width:  42,
+				Height: 13,
+			},
+			expectedErr: ErrIncorrectUse{reason: IncorrectFieldType, field: "Width", kind: reflect.Float64},
+		},
+		{
+			in: Roulette{
+				Value: 0,
+			},
+			expectedErr: ErrIncorrectUse{reason: UnknownRule, rule: "odd"},
+		},
+		{
+			in: Piece{
+				Text: "To be, or not to be, that is the question",
+			},
+			expectedErr: ErrIncorrectUse{reason: UnknownRule, field: "Text", rule: "spell"},
+		},
+		{
+			in: WrongCond1{
+				Value: "",
+			},
+			expectedErr: ErrIncorrectUse{reason: IncorrectCondition, field: "Value", rule: "len", err: atoiErr},
+		},
+		{
+			in: WrongCond2{
+				Value: "",
+			},
+			expectedErr: ErrIncorrectUse{reason: IncorrectCondition, field: "Value", rule: "regexp", err: regexpErr},
+		},
+		{
+			in: WrongCond3{
+				Value: 13,
+			},
+			expectedErr: ErrIncorrectUse{reason: IncorrectCondition, field: "Value", rule: "min", err: atoiErr},
+		},
+		{
+			in: WrongCond4{
+				Value: 13,
+			},
+			expectedErr: ErrIncorrectUse{reason: IncorrectCondition, field: "Value", rule: "min"},
+		},
+		{
+			in: User{},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrStrLen},
+				ValidationError{Field: "Age", Err: ErrIntMin},
+				ValidationError{Field: "Email", Err: ErrStrRegexp},
+				ValidationError{Field: "Role", Err: ErrStrIn},
+			},
+		},
+		{
+			in: &User{},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrStrLen},
+				ValidationError{Field: "Age", Err: ErrIntMin},
+				ValidationError{Field: "Email", Err: ErrStrRegexp},
+				ValidationError{Field: "Role", Err: ErrStrIn},
+			},
+		},
+		{
+			in: User{
+				ID:     "012345678",
+				Age:    51,
+				Email:  "test.mail.ru",
+				Role:   "hacker",
+				Phones: []string{"03"},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrStrLen},
+				ValidationError{Field: "Age", Err: ErrIntMax},
+				ValidationError{Field: "Email", Err: ErrStrRegexp},
+				ValidationError{Field: "Role", Err: ErrStrIn},
+				ValidationError{Field: "Phones", Err: ErrStrLen},
+			},
+		},
+		{
+			in: Response{
+				Code: 418,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Code", Err: ErrIntIn},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			err := Validate(tt.in)
+			require.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
+
+func TestErrIncorrectUse(t *testing.T) {
+	_, atoiErr := strconv.Atoi("oops")
+
+	tests := []struct {
+		name     string
+		in       ErrIncorrectUse
+		expected string
+	}{
+		{
+			name:     "IncorrectKind",
+			in:       ErrIncorrectUse{reason: IncorrectKind, kind: reflect.String},
+			expected: "function only accepts structs; got string",
+		},
+		{
+			name:     "IncorrectFieldType",
+			in:       ErrIncorrectUse{reason: IncorrectFieldType, field: "rate", kind: reflect.Float64},
+			expected: "field `rate` has unsupported type float64",
+		},
+		{
+			name:     "UnknownRule",
+			in:       ErrIncorrectUse{reason: UnknownRule, field: "rate", rule: "odd"},
+			expected: "field `rate` has unknown rule `odd`",
+		},
+		{
+			name:     "IncorrectCondition",
+			in:       ErrIncorrectUse{reason: IncorrectCondition, field: "rate", rule: "len", err: atoiErr},
+			expected: "field `rate` has incorrect condition: `strconv.Atoi: parsing \"oops\": invalid syntax` for rule `len`",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("case %s", tt.name), func(t *testing.T) {
+			require.Equal(t, tt.expected, tt.in.Error())
 		})
 	}
 }
