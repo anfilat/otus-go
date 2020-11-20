9e9f482c5f812c22fe9b21839487dd8e772dfe4a
Andrey Filatkin <anfilat@mail.ru>
2020-11-20 11:52:26 +0300

    sync pool for validation errors

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -3,6 +3,7 @@ package hw09_struct_validator //nolint:golint,stylecheck
 import (
 	"reflect"
 	"strings"
+	"sync"
 )
 
 type ValidationError struct {
@@ -106,17 +107,37 @@ func parseFieldRules(typ reflect.StructField, validateTag string) (fieldRules, e
 	return rules, err
 }
 
+type validationResult struct {
+	errs ValidationErrors
+}
+
+var errsPool = &sync.Pool{
+	New: func() interface{} {
+		return &validationResult{
+			errs: make(ValidationErrors, 0, 16),
+		}
+	},
+}
+
 func validateStruct(sr structRules, value reflect.Value) error {
-	var errs ValidationErrors
+	result := errsPool.Get().(*validationResult)
+	defer func() {
+		result.errs = result.errs[:0]
+		errsPool.Put(result)
+	}()
+
 	for i := 0; i < value.NumField(); i++ {
 		rules := sr[i]
 		if rules == nil {
 			continue
 		}
-		errs = rules.validate(errs, value.Field(i))
+		result.errs = rules.validate(result.errs, value.Field(i))
 	}
-	if errs == nil {
+
+	if len(result.errs) == 0 {
 		return nil
 	}
+	errs := make(ValidationErrors, len(result.errs))
+	copy(errs, result.errs)
 	return errs
 }
